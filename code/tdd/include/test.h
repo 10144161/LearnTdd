bc4c93fead8403b5053c7aa4fcea2482f44da5f5
root <root@localhost.localdomain>
2016-08-07 23:50:00 +0800

    add tdd code by liuhuang

--- code/tdd/include/test.h
+++ code/tdd/include/test.h
@@ -1,7 +1,12 @@
 #ifndef _TEST_H_
 #define _TEST_H_
+#include <string.h>
 
-int returnvaule(int a);
+int ThrowBall(int pin);
+int GetTotalScore();
+int GetFramScore(int framenum);
+int GetCurrentFrame();
+void InitGlobalValue();
 
 #endif
 
--- code/utest/source/tdd_unitest1.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-#include "gtest/gtest.h"
-
-TEST(ReturnValueTest, Zero1)
-{
-    EXPECT_EQ(1, returnvaule(0));
-}
-
-
